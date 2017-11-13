75405c4e7b2b92ff186c76467ee7db2755f3d0f5
jh145216 <31322578+jh145216@users.noreply.github.com>
2017-11-13 11:11:02 -0600

    update Miles per Gallon Calculator

---
+++ Miles per Gallon Calculator/Miles per Gallon Calculator/Form1.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblGallons = New System.Windows.Forms.Label()
+        Me.lblMiles = New System.Windows.Forms.Label()
+        Me.lblMilesPerGallons = New System.Windows.Forms.Label()
+        Me.lblMilesPerGallon = New System.Windows.Forms.Label()
+        Me.txtGallons = New System.Windows.Forms.TextBox()
+        Me.txtMiles = New System.Windows.Forms.TextBox()
+        Me.btnCalculateMPG = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblError = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblGallons
+        '
+        Me.lblGallons.AutoSize = True
+        Me.lblGallons.Location = New System.Drawing.Point(28, 30)
+        Me.lblGallons.Name = "lblGallons"
+        Me.lblGallons.Size = New System.Drawing.Size(92, 26)
+        Me.lblGallons.TabIndex = 0
+        Me.lblGallons.Text = "Gallons of gas the" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "car can hold:"
+        '
+        'lblMiles
+        '
+        Me.lblMiles.AutoSize = True
+        Me.lblMiles.Location = New System.Drawing.Point(28, 95)
+        Me.lblMiles.Name = "lblMiles"
+        Me.lblMiles.Size = New System.Drawing.Size(96, 39)
+        Me.lblMiles.TabIndex = 1
+        Me.lblMiles.Text = "Number of miles it" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "can be driven on a" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "full tank:"
+        '
+        'lblMilesPerGallons
+        '
+        Me.lblMilesPerGallons.AutoSize = True
+        Me.lblMilesPerGallons.Location = New System.Drawing.Point(28, 163)
+        Me.lblMilesPerGallons.Name = "lblMilesPerGallons"
+        Me.lblMilesPerGallons.Size = New System.Drawing.Size(83, 13)
+        Me.lblMilesPerGallons.TabIndex = 2
+        Me.lblMilesPerGallons.Text = "Miles per gallon:"
+        '
+        'lblMilesPerGallon
+        '
+        Me.lblMilesPerGallon.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMilesPerGallon.Location = New System.Drawing.Point(221, 156)
+        Me.lblMilesPerGallon.Name = "lblMilesPerGallon"
+        Me.lblMilesPerGallon.Size = New System.Drawing.Size(109, 20)
+        Me.lblMilesPerGallon.TabIndex = 3
+        '
+        'txtGallons
+        '
+        Me.txtGallons.Location = New System.Drawing.Point(221, 27)
+        Me.txtGallons.Name = "txtGallons"
+        Me.txtGallons.Size = New System.Drawing.Size(109, 20)
+        Me.txtGallons.TabIndex = 4
+        '
+        'txtMiles
+        '
+        Me.txtMiles.Location = New System.Drawing.Point(221, 95)
+        Me.txtMiles.Name = "txtMiles"
+        Me.txtMiles.Size = New System.Drawing.Size(109, 20)
+        Me.txtMiles.TabIndex = 5
+        '
+        'btnCalculateMPG
+        '
+        Me.btnCalculateMPG.Location = New System.Drawing.Point(31, 209)
+        Me.btnCalculateMPG.Name = "btnCalculateMPG"
+        Me.btnCalculateMPG.Size = New System.Drawing.Size(75, 38)
+        Me.btnCalculateMPG.TabIndex = 7
+        Me.btnCalculateMPG.Text = "Calculate" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "MPG"
+        Me.btnCalculateMPG.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(149, 217)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 8
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(255, 217)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblError
+        '
+        Me.lblError.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblError.Location = New System.Drawing.Point(31, 264)
+        Me.lblError.Name = "lblError"
+        Me.lblError.Size = New System.Drawing.Size(299, 23)
+        Me.lblError.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(397, 296)
+        Me.Controls.Add(Me.lblError)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculateMPG)
+        Me.Controls.Add(Me.txtMiles)
+        Me.Controls.Add(Me.txtGallons)
+        Me.Controls.Add(Me.lblMilesPerGallon)
+        Me.Controls.Add(Me.lblMilesPerGallons)
+        Me.Controls.Add(Me.lblMiles)
+        Me.Controls.Add(Me.lblGallons)
+        Me.Name = "Form1"
+        Me.Text = "Miles per Gallon Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblGallons As System.Windows.Forms.Label
+    Friend WithEvents lblMiles As System.Windows.Forms.Label
+    Friend WithEvents lblMilesPerGallons As System.Windows.Forms.Label
+    Friend WithEvents lblMilesPerGallon As System.Windows.Forms.Label
+    Friend WithEvents txtGallons As System.Windows.Forms.TextBox
+    Friend WithEvents txtMiles As System.Windows.Forms.TextBox
+    Friend WithEvents btnCalculateMPG As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblError As System.Windows.Forms.Label
+
+End Class
---
+++ Miles per Gallon Calculator/Miles per Gallon Calculator/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub btnCalculateMPG_Click(sender As Object, e As EventArgs) Handles btnCalculateMPG.Click
+        Dim decGallonhold As Decimal
+        Try
+            decGallonhold = (txtMiles.Text) / (txtGallons.Text)
+            lblMilesPerGallon.Text = decGallonhold.ToString("n2")
+        Catch
+            lblError.Text = "Error be sure to enter non zero numeric values "
+        End Try
+       
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lblError.Text = String.Empty
+        txtGallons.Clear()
+        txtMiles.Clear()
+        lblMilesPerGallon.Text = String.Empty
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
